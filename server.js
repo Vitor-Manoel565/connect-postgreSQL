abfc192d95e36d8378d516aaa9457979b0b07f76
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-11-21 15:37:50 -0300

    add comentarios

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 import client from "pg";
-const {Client} = client;
+const {Client} = client; // pacote pg instalado no projeto, para conectar com o banco de dados
 
-const alunos = new Client({
-  user: "postgres",
-  password: "12345",
-  host: "localhost",
-  port: 5432,
-  database: "aluno",
+const alunos = new Client({ // objeto que representa a conexão com o banco de dados
+  user: "postgres", // usuário do banco de dados
+  password: "12345", // senha do banco de dados
+  host: "localhost", // host do banco de dados (localhost)
+  port: 5432, // porta do banco de dados
+  database: "aluno", // nome do banco de dados
 });
 
-async function connect() {
-    alunos.connect();
-    console.log("Conectado ao banco de dados");
+async function connect() { // função que conecta com o banco de dados
+    alunos.connect(); // conecta com o banco de dados
+    console.log("Conectado ao banco de dados"); // mensagem de confirmação
   try {
-    alunos.query("SELECT * FROM aluno", (error, response) => {
-      console.log(response.rows);
-      alunos.end();
+    alunos.query("SELECT * FROM aluno", (error, response) => { // query que seleciona todos os dados da tabela aluno
+      console.log(response.rows); // mostra os dados da tabela aluno
+      alunos.end(); // encerra a conexão com o banco de dados
     });
-  } catch (error) {
-    console.log("Erro ao conectar ao banco de dados", error);
+  } catch (error) { // caso ocorra algum erro
+    console.log("Erro ao conectar ao banco de dados", error); // mensagem de erro
   }
 }
-connect();
+connect(); // chama a função que conecta com o banco de dados
